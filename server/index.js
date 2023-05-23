bfdc00e71b9fc4f6381faa7debd9f0f527f472af
harshalkodgirwar <harshalkod2003@gmail.com>
2023-05-23 20:56:35 +0530

    initial commit

--- server/index.js
+++ server/index.js
@@ -1,75 +1,91 @@
 const express = require("express");
 const app = express();
-const mysql = require("mysql");
+const { MongoClient } = require("mongodb");
+const { ObjectId } = require("mongodb").ObjectId;
 const cors = require("cors");
 
 app.use(cors());
 app.use(express.json());
 
-const db = mysql.createConnection({
-  user: "root",
-  host: "localhost",
-  password: "prateek666",
-  database: "employeeSystem",
-});
+// Connection URL
+const url = "mongodb+srv://harshalkod:LDdnWxyIfoZvc5Kf@cluster1.kf0oksm.mongodb.net/?retryWrites=true&w=majority"; 
+// Database Name
+const dbName = "emp"; 
 
-app.post("/create", (req, res) => {
+// Collection Name
+const collectionName = "employees"; 
+
+// Function to connect to the MongoDB database
+async function connectToDatabase() {
+  try {
+    // Create a new MongoClient
+    const client = new MongoClient(url);
+
+    // Connect the client to the server
+    await client.connect();
+
+    // Select the database
+    const db = client.db(dbName);
+
+    // Return the database instance
+    return db;
+  } catch (error) {
+    console.error("Error connecting to the database:", error);
+    throw error;
+  }
+}
+
+app.post("/create", async (req, res) => {
   const name = req.body.name;
   const age = req.body.age;
   const country = req.body.country;
   const position = req.body.position;
   const wage = req.body.wage;
 
-  db.query(
-    "INSERT INTO employees (name, age, country, position, wage) VALUES (?,?,?,?,?)",
-    [name, age, country, position, wage],
-    (err, result) => {
-      if (err) {
-        console.log(err);
-      } else {
-        res.send("Values Inserted");
-      }
-    }
-  );
+  try {
+    // Connect to the database
+    const db = await connectToDatabase();
+
+    // Get the collection
+    const collection = db.collection(collectionName);
+
+    // Insert a document
+    const insertResult = await collection.insertOne({
+      name,
+      age,
+      country,
+      position,
+      wage,
+    });
+    console.log("Inserted document:", insertResult.ops[0]);
+    res.send("Values Inserted");
+  } catch (error) {
+    console.log(error);
+    res.status(500).send("Error inserting values");
+  }
 });
 
-app.get("/employees", (req, res) => {
-  db.query("SELECT * FROM employees", (err, result) => {
-    if (err) {
-      console.log(err);
-    } else {
-      res.send(result);
-    }
-  });
+app.get("/employees", async (req, res) => {
+  try {
+    // Connect to the database
+    const db = await connectToDatabase();
+
+    // Get the collection
+    const collection = db.collection(collectionName);
+
+    // Find documents
+    const findResult = await collection.find({}).toArray();
+    res.send(findResult);
+  } catch (error) {
+    console.log(error);
+    res.status(500).send("Error retrieving employees");
+  }
 });
 
-app.put("/update", (req, res) => {
-  const id = req.body.id;
-  const wage = req.body.wage;
-  db.query(
-    "UPDATE employees SET wage = ? WHERE id = ?",
-    [wage, id],
-    (err, result) => {
-      if (err) {
-        console.log(err);
-      } else {
-        res.send(result);
-      }
-    }
-  );
-});
 
-app.delete("/delete/:id", (req, res) => {
-  const id = req.params.id;
-  db.query("DELETE FROM employees WHERE id = ?", id, (err, result) => {
-    if (err) {
-      console.log(err);
-    } else {
-      res.send(result);
-    }
-  });
-});
+
 
 app.listen(3001, () => {
-  console.log("Yey, your server is running on port 3001");
+  console.log("Yay, your server is running on port 3001");
 });
+``
